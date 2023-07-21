266fca28e9b0cb6c808166152306afb9bdb0bb24
ShBind <shivendra.bind@newscapeconsulting.com>
2023-07-21 11:20:28 +0530

    init project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pokemon test ground!
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+```bash
+npm install
+npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="max-w-2xl mx-auto pt-20">
+      <h1> Welcome </h1>
+    </div>
+  );
+}
+
+export default App;
